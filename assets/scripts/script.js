1a2de224f32e4e708a952a40ece9b72ed6913b2c
Remy Gosselin <gosselin.remy@gmail.com>
2022-09-05 15:00:16 +0400

    avancement

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,6 +7,9 @@ class Request {
         this.title = title;
         this.div = div;
     }
+    static retrieveOneId(url2){
+        return `url2.results[0]['url]`;
+    }
 
     static retrieveId(e){
         const urlFilm = `http://localhost:8000/api/v1/titles/${e['path'][1].id}`;
@@ -56,15 +59,48 @@ class Request {
             })
     }
 
+    overview(){
+        fetch(this.url)
+            .then(values => values.json())
+            .then(infos => {
+                console.log(infos.results[0]);
+
+                document.getElementById('video').innerHTML +=
+                    `<img class="overview__img" src="${infos.results[0]['image_url']}"></img>`
+        
+                document.getElementById('res').innerHTML +=
+                    `<p class="overview__titre">${infos.results[0]['title']}</p>`
+
+                document.getElementById('res').innerHTML +=
+                    `<button class="overview__infos">Informations</button>`
+
+                document.getElementById('res').innerHTML +=
+                    `<div class="overview__espace"></div>`
+
+                
+                
+                fetch(infos.results[0]['url'])
+                    .then(values2 => values2.json())
+                    .then(infos2 => {
+                        console.log(infos2['description']);
+                        document.getElementById('res').innerHTML +=
+                            `<p class="overview__desc">Description: ${infos2['description']}</p>`
+                    })
+            })
+    }
+
     run(){
         fetch(this.url)
             .then(values => values.json())
             .then(infos => {
                 let div = document.getElementById(this.div);
                 let nameIdImage = `images_${this.div}`
+                
+                //document.getElementById(video).innerHTML +=
+                //    `<img src=${infos.results[0].img_url}></img>`
 
                 //div.innerHTML += `<h2 id = t_${this.div}>${this.title}</h2>`;
-                div.innerHTML += `<div id='t_${this.div}'>${this.title}</div>`
+                div.innerHTML += `<div id='t_${this.div}'>${this.title}</div>`;
                 
                 div.innerHTML += `<div id = ${nameIdImage}></div>`;
 
@@ -81,6 +117,8 @@ let bestMovieRequest = new Request("http://localhost:8000/api/v1/titles/?sort_by
 let bestActionRequest = new Request("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score%2C-votes&genre=action&page_size=7%22",  "Meilleurs films d'action", "best_action");
 let bestAnimationRequest = new Request("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score%2C-votes&genre=animation&page_size=7%22",  "Meilleurs films d'animation", "best_animation");
 let bestSciFiRequest = new Request("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score%2C-votes&genre=sci-fi&page_size=7%22",  "Meilleurs films de Sci-Fi", "best_sf");
+
+bestMovieRequest.overview();
 
 bestMovieRequest.run();
 
